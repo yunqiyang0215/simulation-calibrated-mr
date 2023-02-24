9d775f28ec6c035b79fe11b5209d32179df83044
yunqiyang0215 <yunqiyang@uchicago.edu>
2023-02-24 10:42:21 -0600

    revise script

--- analysis/gaussian_rho.R
+++ analysis/gaussian_rho.R
@@ -1,6 +1,5 @@
-
 # Simulation 2: check the derivation of variance reduction in Gaussian case (x1, x2 joint Gaussian)
-# Check how rho affect VR.
+# Check how rho affect VR. 
 
 
 
@@ -10,37 +9,38 @@ library(mvtnorm)
 t1 <- proc.time()
 
 seeds <- c(1:1e5)
-N = 5e4
-rhos <- c(0.1, 0.3, 0.5, 0.8, 1)
+n = 5e3
+rhos <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
 b = c(1,3,0.1)
 sigma = 1
 res.all <- list()
-r = 0.1
+r = 0
 
 for (j in 1:length(rhos)){
   rho <- rhos[j]
-  n = N*rho
-
-  res <- matrix(NA, ncol = 2, nrow = length(seeds))
-  colnames(res) <- c("gamma.tilde", "gamma.raw")
-
+  N = round(n/rho)
+  
+  res <- matrix(NA, ncol = 3, nrow = length(seeds))
+  colnames(res) <- c("gamma.tilde", "gamma.tilde2", "gamma.raw")
+  
   for (i in 1:length(seeds)){
     set.seed(i)
     # simulate data
     datN <- sim_dat(N, b, r = r, sigma = sigma)
     datn <- sim_dat(n, b, r = r, sigma = sigma)
-
+    
     # fit 3 models
     fit.true <- lm(y ~ x1 + x2, dat = data.frame(datn))
     fitN <- lm(y ~ x1, dat = data.frame(datN))
     fitn <- lm(y ~ x1, dat = data.frame(datn))
-
+    
     bhat <- coef(fit.true)[-1]
     ahat.N <- coef(fitN)[-1]
     ahat.n <- coef(fitn)[-1]
-
+    
     res[i, 1] <- calibrated_estimator(bhat, ahat.N, ahat.n, rho = n/N, b[3], r = r, sigma = sigma)
-    res[i, 2] <- bhat[1] - bhat[2] # \hat b1 - \hat b2
+    res[i, 2] <- calibrated_estimator(bhat, 3, ahat.n, rho = n/N, b[3], r = r, sigma = sigma)    
+    res[i, 3] <- bhat[1] - bhat[2] # \hat b1 - \hat b2
   }
   res.all[[j]] <- res
 }
@@ -49,4 +49,5 @@ t2 <- proc.time()
 print(t2 - t1)
 
 saveRDS(res.all, "/project2/mstephens/yunqiyang/calibrated_mr/sim_gaussian/gaussian_rho.rds")
+
 
