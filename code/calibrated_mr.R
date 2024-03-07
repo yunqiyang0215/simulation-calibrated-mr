b529a3e0bef45e792532905afa45fa008ee5d7be
yunqi yang <yunqiyang@uchicago.edu>
2024-03-07 15:31:45 -0600

    edit compute_sumstat

--- code/calibrated_mr.R
+++ code/calibrated_mr.R
@@ -1,16 +1,20 @@
 # @param Y: a vector contains response variable values
 # @param X1: a vector contains transmitted allele values
-compute_sumstat <- function(Y, X1){
+compute_sumstat <- function(Y, X1, family = "gaussian"){
   n <- length(Y)
-  fit <- lm(Y ~ X1)
-  fit_summary = summary(fit)
+  if (family == "gaussian"){
+    fit <- lm(Y ~ X1)
+  }
   
+  if (family == "binomial"){
+    fit <- glm(Y ~ X1, family = "binomial")
+  }
+  fit_summary = summary(fit)
   bhat = fit_summary$coefficients[2, "Estimate"]
   std = fit_summary$coefficients[2, "Std. Error"]
   resid <- fit$residuals
   return(list(bhat = bhat, std = std, resid = resid))
 }
-
 
 # @param Y: a vector contains response variable values
 # @param X1: a vector contains transmitted allele values
